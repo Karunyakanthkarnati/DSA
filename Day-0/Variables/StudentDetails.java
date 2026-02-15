9fd5d5070e02895aca7b8fcd108a64737ef92df1
Karunyakanthkarnati <23pa1a05a6@vishnu.edu.in>
2026-02-15 23:20:34 +0530

    Create Main class to output student information
    Implemented a Main class to display student details including name, age, and percentage.

---
+++ Day-0/Variables/StudentDetails.java
@@ -0,0 +1,10 @@
+class Main{
+    public static void main(String[] args){
+        String name = "Karunya";
+        int age = 18;
+        double percentage = 97.5;
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(percentage);
+    }
+}
