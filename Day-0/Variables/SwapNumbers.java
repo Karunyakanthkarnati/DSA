e5d8f361c6ce6a5c3290c14145e0153638b67a06
Karunyakanthkarnati <23pa1a05a6@vishnu.edu.in>
2026-02-15 23:22:23 +0530

    Swapping of two numbers

---
+++ Day-0/Variables/SwapNumbers.java
@@ -0,0 +1,12 @@
+class SwapNumbers{
+    public static void main(String[] args){
+        int temp;
+        int a = 50;
+        int b = 60;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a = "+ a);
+        System.out.println("b = "+b);
+    }
+}
