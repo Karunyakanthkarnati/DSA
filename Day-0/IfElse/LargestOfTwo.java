eec8d098cbf96c8d257803beed098ef3300e07eb
Karunyakanthkarnati <23pa1a05a6@vishnu.edu.in>
2026-02-16 21:01:15 +0530

    find the largest of two numbers

---
+++ Day-0/IfElse/LargestOfTwo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class LargestOfTwo{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        if(num1 >= num2){
+            System.out.println("the largest number is "+ num1);
+        }
+        else{
+            System.out.println("the largest number is "+num2);
+        }
+    }
+}
