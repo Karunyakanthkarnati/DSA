772775290f1c380b3772b11d05df98ebbb3b3984
Karunyakanthkarnati <23pa1a05a6@vishnu.edu.in>
2026-02-18 23:40:36 +0530

    finding second largest element

---
+++ Day-2_Arrays/SecondLargest.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+class SecondLargest{
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int largest = Integer.MIN_VALUE;
+        int second_largest = Integer.MIN_VALUE;
+        for(int i = 0;i<n;i++){
+            if(arr[i]>largest){
+                second_largest = largest;
+                largest = arr[i];
+            }
+            else if(arr[i]>second_largest){
+                second_largest = arr[i];
+            }
+        }
+        if(second_largest == Integer.MIN_VALUE){
+            System.out.println("Second largest number does not exist");
+        } 
+        else{
+            System.out.println("The second largest number is "+second_largest);
+        }
+        
+    }
+}
