b8b1002da3b8db2cca76e9f7ef348620b4c5dfc0
Karnati Karunya Kanth <23pa1a05a6@vishnu.edu.in>
2026-02-20 19:55:41 +0530

    Implement Selection Sort to find kth minimum element

---
+++ Day-3/Sorting/SelectionSortKth.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class SelectionSortKth{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no.of elements : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter the index : ");
+        int k = sc.nextInt();
+        if (k <= 0 || k > n) {
+            System.out.println("Invalid k");
+            return;
+        }
+
+        for(int i = 0;i<k;i++){
+            int min = i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[min]){
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.println("the "+k+"th minimum element is "+arr[k-1]);
+    }
+}
