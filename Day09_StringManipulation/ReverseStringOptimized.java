deaeea4ce7d410a4652d197f9f1aaece820290f0
Karnati Karunya Kanth <23pa1a05a6@vishnu.edu.in>
2026-02-28 21:51:03 +0530

    Create ReverseStringOptimized.java

---
+++ Day09_StringManipulation/ReverseStringOptimized.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class ReverseStringOptimized {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the string:");
+        String str = sc.nextLine();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            sb.append(str.charAt(i));
+        }
+
+        System.out.println("Reversed string: " + sb.toString());
+    }
+}
