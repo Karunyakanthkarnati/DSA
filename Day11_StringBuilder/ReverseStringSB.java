e550eb086df5d2fe7b7aee0e4a8148b3bbe3a0dc
Karnati Karunya Kanth <23pa1a05a6@vishnu.edu.in>
2026-03-02 21:23:54 +0530

    Create ReverseStringSB.java

---
+++ Day11_StringBuilder/ReverseStringSB.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class ReverseStringSB{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        StringBuilder sb = new StringBuilder(str);
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
