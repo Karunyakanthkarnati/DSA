e7fff97ca74ae8012ff4e4c8124aae19af20f3b5
Karnati Karunya Kanth <23pa1a05a6@vishnu.edu.in>
2026-03-02 21:24:41 +0530

    Create BuildSentence.java

---
+++ Day11_StringBuilder/BuildSentence.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class BuildSentence{
+    public static void main (String[] args) {
+        StringBuilder sb = new StringBuilder("ramu");
+        sb.append(" is");
+        sb.append(" good");
+        sb.append(" boy");
+        System.out.println(sb);
+    }
+}
